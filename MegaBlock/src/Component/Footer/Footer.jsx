94bd8528ff80c6c4a3f0d2c77b11633efc08e9ea
Paban-1 <rpaban89@gmail.com>
2025-09-10 22:07:25 +0530

    Added components UI

--- MegaBlock/src/Component/Footer/Footer.jsx
+++ MegaBlock/src/Component/Footer/Footer.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Logo } from "../index";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="fixed border w-full py-4 bottom-0 flex justify-between px-8">
+      <h2>This is Footer</h2>
+      <Logo />
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- MegaBlock/src/Component/Header/Header.jsx
+++ MegaBlock/src/Component/Header/Header.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import { Container, Logo, LogOutBtn } from "../index";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
+  const navItems = [
+    { name: "Home", slug: "/", active: true },
+    { name: "Login", slug: "/login", active: !authStatus },
+    { name: "Sigup", slug: "/sigup", active: !authStatus },
+    { name: "Post", slug: "/all-post", active: !authStatus },
+    { name: "Addpost", slug: "/add-post", active: !authStatus },
+  ];
   return (
-    <div>Header</div>
-  )
-}
+    <header className="py-3 shadow bg-gray-500 fixed top-0 w-full">
+      <Container>
+        <nav className="flex">
+          <div className="mr-4">
+            <Link to="/">
+              <Logo Width="70px" />
+            </Link>
+          </div>
+          <ul className="flex text-[20px] ml-auto gap-4">
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.name} className="flex gap-4">
+                  <button onClick={() => navigate(item.slug)}>
+                    {item.name}
+                  </button>
+                </li>
+              ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogOutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+      </Container>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
