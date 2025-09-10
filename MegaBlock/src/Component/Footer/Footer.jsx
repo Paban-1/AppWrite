203d331df59db066dd9edb5a286a3a7a613b5449
Paban-1 <rpaban89@gmail.com>
2025-09-10 22:22:32 +0530

    change Footer

--- MegaBlock/src/Component/Footer/Footer.jsx
+++ MegaBlock/src/Component/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import { Logo } from "../index";
 const Footer = () => {
   return (
     <div className="fixed border w-full py-4 bottom-0 flex justify-between px-8">
-      <h2>This is Footer</h2>
+      <h2 className="text-4xl">This is Footer</h2>
       <Logo />
     </div>
   );
